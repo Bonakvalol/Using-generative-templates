d98ad31997a06995ff39c3cf6f2ff86e27adf312
Бонаква <zefirax89@gmail.com>
2025-03-31 07:24:17 +0300

    Add files via upload

---
+++ Using generative templates3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public abstract class Vehicle
+{
+    public abstract void Drive();
+}
+
+public class Car : Vehicle
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Автомобиль едет");
+    }
+}
+
+public class Bicycle : Vehicle
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Велосипед едет");
+    }
+}
+
+public abstract class VehicleFactory
+{
+    public abstract Vehicle CreateVehicle();
+}
+
+public class CarFactory : VehicleFactory
+{
+    public override Vehicle CreateVehicle()
+    {
+        return new Car();
+    }
+}
+
+public class BicycleFactory : VehicleFactory
+{
+    public override Vehicle CreateVehicle()
+    {
+        return new Bicycle();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        VehicleFactory carFactory = new CarFactory();
+        Vehicle car = carFactory.CreateVehicle();
+        car.Drive();
+        VehicleFactory bicycleFactory = new BicycleFactory();
+        Vehicle bicycle = bicycleFactory.CreateVehicle();
+        bicycle.Drive();
+    }
+}
